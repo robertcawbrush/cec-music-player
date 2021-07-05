ef83ee44f0c6901f0e066637219b2ad2fecabf00
robertcawbrush <robertcawbrush@gmail.com>
2021-07-05 16:11:51 -0500

    add env

--- cec.py
+++ cec.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from dotenv import load_dotenv
+
 app = Flask(__name__)
 
+load_dotenv()
+
 @app.route('/')
-def hello_world():
-    return 'Hello, CEC!'
+def app():
+    return f'app running on {LOCALHOST}:{PORT}'
+
+# listen for message from discord bot then grab the file from the stream or maybe just grab it off the file system of the discord bot
+
+@app.route('/audio')
+def audio():
+    return 'we runnin'
